0aac1610fcc51e838c0534183c69a48aaf648e4b
ariaandika <andikaakun17@gmail.com>
2025-02-18 17:43:51 +0700

    serve css statically

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,5 @@
 use axum::Router;
+use tower_http::services::ServeFile;
 
 pub mod page;
 pub mod error;
@@ -8,6 +9,7 @@ pub mod home;
 
 pub fn routes() -> Router {
     Router::new()
+        .route_service("/output.css", ServeFile::new("templates/output.css"))
         .merge(auth::routes())
         .merge(home::routes())
 }
