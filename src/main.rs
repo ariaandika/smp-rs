c977c6c87e88fe5746aa0f76c5a099ece5e17ef8
ariaandika <andikaakun17@gmail.com>
2025-03-06 20:02:51 +0700

    cleanup

--- src/main.rs
+++ src/main.rs
@@ -1,38 +1,21 @@
-use smp_rs::error::{env, SetupError};
-use std::{net::TcpListener as StdTcp, process::exit};
-use tokio::net::TcpListener;
-use tracing_subscriber::EnvFilter;
-
-const ADDR: &str = "0.0.0.0:3000";
+use std::{io, process};
+use vice::router::{get, Router};
 
 fn main() {
     if let Err(err) = app() {
         eprintln!("{err}");
-        exit(1);
+        process::exit(1);
     }
 }
 
-fn app() -> Result<(), SetupError> {
+fn app() -> io::Result<()> {
     dotenvy::dotenv().ok();
+    Router::new()
+        .route("/", get(index))
+        .listen("0.0.0.0:3000")
+}
 
-    env("JWT_SECRET")?;
-    env("DATABASE_URL")?;
-
-    tracing_subscriber::fmt()
-        .with_env_filter(EnvFilter::from_default_env())
-        .init();
-
-    let tcp = StdTcp::bind(ADDR).map_err(SetupError::Tcp)?;
-    tcp.set_nonblocking(true)?;
-
-    tokio::runtime::Builder::new_multi_thread()
-        .enable_all()
-        .build()?
-        .block_on(async move {
-            let app = smp_rs::setup()?;
-            let tcp = TcpListener::from_std(tcp).map_err(SetupError::Tcp)?;
-            tracing::info!("listening: {}", tcp.local_addr().unwrap());
-            axum::serve(tcp, app).await.map_err(Into::into)
-        })
+async fn index() -> &'static str {
+    "Nice"
 }
 
