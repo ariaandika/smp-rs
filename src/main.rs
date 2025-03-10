f7f75880f7624481812e0a24ef9e66cfbd2db616
ariaandika <andikaakun17@gmail.com>
2025-03-10 09:54:49 +0700

    bind preview and rust server

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,10 @@
 use std::{io, process};
 
+use axum::{response::IntoResponse, routing::get, Router};
+use rinja::Template;
+use tokio::net::TcpListener;
+use tower_http::services::ServeFile;
+
 fn main() {
     dotenvy::dotenv().ok();
     if let Err(err) = app() {
@@ -9,6 +14,29 @@ fn main() {
 }
 
 fn app() -> io::Result<()> {
-    todo!()
+    let tcp = std::net::TcpListener::bind("0.0.0.0:3000")?;
+    tcp.set_nonblocking(true)?;
+
+    let routes = Router::new()
+        .nest_service("/assets/output.css", ServeFile::new("assets/output.css"))
+        .route("/", get(page));
+
+    tokio::runtime::Builder::new_multi_thread()
+        .enable_io()
+        .build()?
+        .block_on(async {
+            let tcp = TcpListener::from_std(tcp)?;
+            axum::serve(tcp, routes).await
+        })
+}
+
+async fn page() -> impl IntoResponse {
+    ([("content-type",Page::MIME_TYPE)],Page { }.render().unwrap())
+}
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct Page {
+
 }
 
